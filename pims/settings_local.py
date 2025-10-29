095e7de5cecbdde339029be8d94919c66f1a4d55
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2025-10-29 15:06:04 +0530

    worked

--- pims/settings_local.py
+++ pims/settings_local.py
@@ -84,8 +84,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pims.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 #
 # DATABASES = {
 #     'default': {
